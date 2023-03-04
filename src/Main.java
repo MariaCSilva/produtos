109078020afff42fa19f21146e186bf2c125ae2c
Maria Silva <maria.correia.s@gmail.com>
2023-03-04 18:58:41 -0300

    Criando a classe Produto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import models.Produto;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+        produto1.setId(1);
+        produto1.setDescricao("Celular");
+    }
+}
