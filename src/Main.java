a2fa016b57c3523e9ebc80ad5413276e28eb1ca5
Maria Silva <maria.correia.s@gmail.com>
2023-03-04 19:12:46 -0300

    Solicitando do usuario os dados do produto

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 import models.Produto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Produto produto1 = new Produto();
-        produto1.setId(1);
-        produto1.setDescricao("Celular");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual a descrição que você deseja dar ao novo produto: ");
+        String descricao = scanner.nextLine();
+
+        System.out.println("Qual o ID você deseja dar ao novo produto: ");
+        int id = scanner.nextInt();
+
+        Produto novoProduto =  new Produto();
+        novoProduto.setId(id);
+        novoProduto.setDescricao(descricao);
+
+        System.out.println("Produto criado com sucesso: ");
+        System.out.println("----ID: "+novoProduto.getId());
+        System.out.println("----Descrição: "+novoProduto.getDescricao());
     }
 }
