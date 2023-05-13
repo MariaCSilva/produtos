41dda5885434d0a9b994b1004cb6041985a88d1a
Maria Silva <maria.correia.s@gmail.com>
2023-05-13 19:08:12 -0300

    Gestão de estoque:  1 - Adicionando produto ao estoque  2 - Listando produtos do estoque

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
+import db.EstoquesDB;
 import db.ProdutosDB;
 import db.UsuariosDB;
-import models.Admin;
-import models.Cliente;
-import models.Produto;
-import models.Usuario;
+import models.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,6 +11,7 @@ import java.util.Scanner;
 public class Main {
     static ProdutosDB produtosDB = new ProdutosDB();
     static UsuariosDB usuariosDB = new UsuariosDB();
+    static EstoquesDB estoquesDB = new EstoquesDB();
     public static void main(String[] args) throws Exception {
         System.out.println("---PEDIDO DE VENDAS");
         int option;
@@ -23,6 +22,8 @@ public class Main {
             System.out.println("3 - Cadastrar usuário ADMINISTRADOR");
             System.out.println("4 - Cadastrar usuário CLIENTE");
             System.out.println("5 - Listar todos os usuários");
+            System.out.println("6 - Cadastrar novo estoque");
+            System.out.println("7 - Listar todos os todos os estoques");
             System.out.println("0 - Sair");
             System.out.print("Qual operação você deseja realizar: ");
             option = scanner.nextInt();
@@ -91,12 +92,46 @@ public class Main {
             case 5:{
                 System.out.println("--------------------------------------------");
                 System.out.println("-------LISTANDO USUÁRIOS CADASTRADOS--------");
-
+                System.out.println("--------------------------------------------");
                 for (Usuario usuario: usuariosDB.getUsuarioList()){
                     System.out.println("ID: " + usuario.getId());
                     System.out.println("ID: " + usuario.getNome());
                     System.out.println("ID: " + usuario.getTipoUsuario());
                     System.out.println("--------------------------------------------");
+                }
+                break;
+            }
+            case 6:{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("--------------------------------------------");
+                System.out.println("-------LISTANDO USUÁRIOS CADASTRADOS--------");
+                System.out.println("--------------------------------------------");
+                System.out.print("Qual o identificador do estoque: ");
+                String id = scanner.next();
+                System.out.print("Qual o produto que será adicionado ao estoque (informe o ID): ");
+                int produtoID = scanner.nextInt();
+                Produto produto = produtosDB.getProdutoPorID(produtoID);
+                System.out.println("PRODUTO ID:        "+ produto.getId());
+                System.out.println("PRODUTO DESCRIÇÃO: "+ produto.getDescricao());
+                System.out.println("PRODUTO VALIDADE: "+ produto.getDataValidade());
+
+                System.out.print("Informe a quantidade de produto a ser adicionado no estoque: ");
+                int quantidade = scanner.nextInt();
+                Estoque novoEstoque = new Estoque(id, produto,quantidade);
+                estoquesDB.addEstoquesDBMap(novoEstoque);
+                break;
+            }
+            case 7:{
+                System.out.println("--------------------------------------------");
+                System.out.println("-------LISTANDO ESTOQUES CADASTRADOS--------");
+                System.out.println("--------------------------------------------");
+
+                for (Estoque estoque: estoquesDB.getEstoquesList()){
+                    System.out.println("----ID: "+estoque.getId());
+                    System.out.println("----PRODUTO: "+estoque.getProduto().getDescricao());
+                    System.out.println("----PREÇO: "+estoque.getProduto().getPreco());
+                    System.out.println("----QUANTIDADE: "+estoque.getQuantidade());
+                    System.out.println("------------------------------------------------");
                 }
                 break;
             }
--- src/db/ProdutosDB.java
+++ src/db/ProdutosDB.java
@@ -3,16 +3,26 @@ package db;
 import models.Produto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProdutosDB {
-    private List<Produto> produtosList = new ArrayList<>();
+    private Map<Integer, Produto> produtosMap = new HashMap<>();
 
     public List<Produto> getProdutosList() {
-        return produtosList;
+        List<Produto> produtos = new ArrayList<>();
+        for (Map.Entry<Integer,Produto> produto : produtosMap.entrySet()){
+            produtos.add(produto.getValue());
+        }
+        return produtos;
     }
 
     public void addNovoProduto(Produto produto) {
-        this.produtosList.add(produto);
+        this.produtosMap.put(produto.getId(), produto);
+    }
+
+    public Produto getProdutoPorID(int  id) {
+        return this.produtosMap.get(id);
     }
 }
